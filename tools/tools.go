058782fd8db21842ffb4c97343e2363f594c6403
Mads Rantala Lundholm <mads@madslundholm.dk>
2026-02-08 15:02:44 +0100

    chore: test using opentofu

--- tools/tools.go
+++ tools/tools.go
@@ -9,10 +9,10 @@ import (
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
 
-// Format Terraform code for use in documentation.
-// If you do not have Terraform installed, you can remove the formatting command, but it is suggested
+// Format OpenTofu code for use in documentation.
+// If you do not have OpenTofu installed, you can remove the formatting command, but it is suggested
 // to ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ../examples/
+//go:generate tofu fmt -recursive ../examples/
 
 // Generate documentation.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name aws-landing-zone-config
